ed2febe9d88896380b4685f6c1c37882b928cd05
gredix <pierre.chop@gmail.com>
2017-02-15 14:13:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,75 +20,76 @@ $(document).ready(function(){
     var batUn_costBase = 1.2;
     var batUn_nombre = 0;
     var batUn_prix = 12;
-    
-    var batUn_mult = 1;
-    var batUn_mult_prix = 500;
+	
+	var megaL_initial = 666;
+    var megaL_costBase = 1.2;
+    var megaL_nombre = 0;
+    var megaL_prix = 800;
     
 
     $('#click').click(function(){
         total_click += powerClick;
-        $("#total_clicks").text(total_click);
         buttonUpdate();
     });
     
-    $("#buy_auto_click").click(function(){
-        if(total_click >= prixAuto){
-            
-            total_click -= Math.ceil((Math.pow(5, nbAuto)-1)/(5 - 1));
-            nbAuto++;
-            prixAuto = Math.ceil((Math.pow(5, nbAuto)-1)/(5 - 1));
-            
-            $("#buy_auto_click").text("Buy for " + prixAuto);
-            $("#autoclicker_level").text("lvl " + (nbAuto-1));
-            $("#total_clicks").text(total_click);
+    $("#buy_mega_clicker").click(function(){
+	
+	
+        if(total_click >= megaL_prix){
+
+            // (prix_dep^num_upgrade - 1) / (prix_dep - 1)
+            total_click -= Math.ceil(megaL_initial*(Math.pow(megaL_costBase, megaL_nombre+1)));
+            megaL_nombre++;
+            megaL_prix = Math.ceil(megaL_initial*(Math.pow(megaL_costBase, megaL_nombre+1)));
+
+            $("#buy_mega_clicker").text("Buy for " + megaL_prix);
+            $("#mega_clicker_level").text("lvl " + (megaL_nombre));
             
             buttonUpdate();
         }
-    });
+});
     
     $("#increase_clicks").click(function(){
+	// augmentation du pouvoir de click
+	
         if(total_click >= power_prix){
             
             // (prix_dep^num_upgrade - 1) / (prix_dep - 1)
-            total_click -= Math.ceil(power_initial*(Math.pow(power_costBase, power_nombre+1)));
+            total_click -= Math.ceil(batUn_initial*(Math.pow(power_costBase, power_nombre+1)));
             power_nombre++;
             powerClick++;
-            power_prix = Math.ceil(power_initial*(Math.pow(power_costBase, power_nombre+1)));
+            power_prix = Math.ceil(batUn_initial*(Math.pow(power_costBase, power_nombre+1)));
             
             $("#increase_clicks").text("Buy for " + power_prix);
-            $("#power_level").text("lvl " + (power_nombre));
-            $("#total_clicks").text(total_click);
-            $("#power_clicks").text(powerClick);
+            $("#power_level").text("lvl " + (power_nombre-1));
+			
             buttonUpdate();
         }
     });
     
     $("#auto_liker").click(function(){
+	// augmentation du auto click
+	
         if(total_click >= batUn_prix){
 
             // (prix_dep^num_upgrade - 1) / (prix_dep - 1)
             total_click -= Math.ceil(batUn_initial*(Math.pow(batUn_costBase, batUn_nombre+1)));
             batUn_nombre++;
-            $("#clicks_per_second").text(batUn_nombre*batUn_mult);
+            $("#clicks_per_second").text(batUn_nombre);
             batUn_prix = Math.ceil(batUn_initial*(Math.pow(batUn_costBase, batUn_nombre+1)));
 
             $("#auto_liker").text("Buy for " + batUn_prix);
             $("#auto_liker_level").text("lvl " + (batUn_nombre));
-            $("#total_clicks").text(total_click);
             
             buttonUpdate();
         }
-    });
-    
-    $("#mult1").click(function() {
-        if(total_click >= batUn_mult_prix){
-            batUn_mult++;
-            $("#mult1").hide();
-        }
-    });
-    
+});
     
     var buttonUpdate = function(){
+	// mise a jour des stats
+	
+		$("#total_clicks").text(total_click);
+		
         if(total_click >= prixAuto){
             $("#buy_auto_click").removeClass("btn-danger");
             $("#buy_auto_click").addClass("btn-success");
@@ -110,10 +111,14 @@ $(document).ready(function(){
             $("#auto_liker").addClass("btn-danger");
             $("#auto_liker").removeClass("btn-success");
         }
-        if(total_click >= batUn_mult_prix && batUn_mult < 2){
-            $("#mult1").show();
+        if(total_click >= megaL_prix){
+            $("#buy_mega_clicker").removeClass("btn-danger");
+            $("#buy_mega_clicker").addClass("btn-success");
+        } else {
+            $("#buy_mega_clicker").addClass("btn-danger");
+            $("#buy_mega_clicker").removeClass("btn-success");
         }
-    };
+		};
     
     function ajoutAuto(number){
         total_click += number;
@@ -122,7 +127,8 @@ $(document).ready(function(){
 
     window.setInterval(function(){
 
-        ajoutAuto(batUn_nombre*batUn_mult);
+        ajoutAuto(batUn_nombre);
+		ajoutAuto(megaL_nombre*100);
 
         buttonUpdate();
 
