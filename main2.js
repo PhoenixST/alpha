ad58986074319324f50389bbb2b33099390235d6
gredix <pierre.chop@gmail.com>
2017-02-16 09:58:01 +0100

    trying object

---
+++ main2.js
@@ -0,0 +1,50 @@
+var money = 0;
+
+var click = {
+  power = 1,
+  click : function(){
+    money += power;
+  }
+};
+
+var autoclick = {
+  name : "Super clicker",
+  baseInc : 1,
+  baseCost : 1.5,
+  prix : 15,
+  basePower : 10,
+  nombre : 0,
+  ajout : function() {
+      if ( money >= this.prix ){
+        money -= this.prix;
+        this.nombre++;
+        $("#clicks_per_second").text(this.nombre);
+        this.prix = Math.ceil( this.basePower * Math.pow( this.baseCost , this.nombre + 1 ));
+        $("#auto_liker").text("Buy for " + this.prix);
+        $("#auto_liker_level").text("lvl " + (this.nombre));
+      }
+  }
+};
+
+
+var buttonUpdate = function(){
+  // mise a jour des stats
+  $("#total_clicks").text(money);
+
+  if(money >= autoclick.prix){
+      $("#auto_liker").removeClass("btn-danger");
+      $("#auto_liker").addClass("btn-success");
+  } else {
+      $("#auto_liker").addClass("btn-danger");
+      $("#auto_liker").removeClass("btn-success");
+  }
+};
+
+
+$(document).ready(function(){
+
+  $('#click').click(powerClick.);
+  $("#auto_liker").click(autoclick.ajout);
+
+
+});
